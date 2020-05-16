55e4828b08ae4bb3cb7f15795d7383c3140f608d
Benjamin Han <laxatives@users.noreply.github.com>
2020-05-16 15:42:19 -0700

    SARSA

--- mobility_on_demand/model/agent.py
+++ mobility_on_demand/model/agent.py
@@ -4,69 +4,87 @@
 # @Date:   2020-05
 import collections
 import os
-import random
+from importlib import util as import_util
+from typing import Dict, Set
+
+if import_util.find_spec('model'):
+    # Use  this for submission
+    from model import utils
+    from model.utils import DispatchCandidate, Request, RepositionData
+elif import_util.find_spec('mobility_on_demand'):
+    # Use this for development
+    from mobility_on_demand.model import utils
+    from mobility_on_demand.model.utils import DispatchCandidate, Request, RepositionData
+else:
+    raise RuntimeError()
+
 
 
 MODEL_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'TODO')
 
 
-class Agent(object):
-    """ Agent for dispatching and reposition """
+class Agent:
+    """ Agent for dispatching and repositioning drivers for the 2020 ACM SIGKDD Cup Competition """
 
-    def __init__(self):
-        """ Load your trained model and initialize the parameters """
-        pass
+    def __init__(self, alpha=2/(5*60), gamma=0.9, h3_resolution=8, idle_cost=-2/(60*60)):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.h3_resolution = h3_resolution
+        self.idle_cost = idle_cost
 
-    def dispatch(self, dispatch_observ):
-        """ Compute the assignment between drivers and passengers at each time step
-    :param dispatch_observ: a list of dict, the key in the dict includes:
-        order_id, int
-        driver_id, int
-        order_driver_distance, float
-        order_start_location, a list as [lng, lat], float
-        order_finish_location, a list as [lng, lat], float
-        driver_location, a list as [lng, lat], float
-        timestamp, int
-        order_finish_timestamp, int
-        day_of_week, int
-        reward_units, float
-        pick_up_eta, float
+        self.state_values = collections.defaultdict(int)  # Expected reward for a driver in each geohash
+        self.last_dispatch = {}
+        self.last_reposition = {}
 
-    :return: a list of dict, the key in the dict includes:
-        order_id and driver_id, the pair indicating the assignment
-    """
-        dispatch_observ.sort(key=lambda od_info: od_info['reward_units'], reverse=True)
+    def dispatch(self, dispatch_input):
+        """ Compute the assignment between drivers and passengers at each time step """
+        drivers, requests, candidates = utils.parse_dispatch(dispatch_input, self.h3_resolution)
+        assigned_driver_ids = set()  # type: Set[str]
+        dispatch = dict()  # type: Dict[str, DispatchCandidate]
 
-        orders = collections.OrderedDict()
-        for od in dispatch_observ:
-            order_id = od['order_id']
-            if order_id not in orders:
-                orders[order_id] = []
-            orders[order_id].append(od)
+        # Greedily match highest reward requests first
+        for request in sorted(requests.values(), key=lambda x: x.reward, reverse=True):  # type: Request
+            best_candidate: DispatchCandidate = None
+            best_update: int = 0  # Wait if value is negative
 
-        assigned_drivers = set()
-        dispatch_action = []
+            for candidate in sorted(candidates[request.request_id], key=lambda x: x.distance, reverse=True):  # type: DispatchCandidate
+                if candidate.driver_id in assigned_driver_ids:
+                    continue
 
-        # Iterate highest reward first
-        while orders:
-            order_id, ods = orders.popitem(last=False)
+                driver = drivers[candidate.driver_id]
+                request = requests[candidate.request_id]
+                v0 = self.state_values[driver.location]  # Value of the driver current position
+                v1 = self.state_values[request.end_loc]  # Value of the proposed new position
+                reward = request.reward
 
-            # Greedily match low pick up ETA
-            sorted_candidates = sorted(ods, key=lambda x: x['pick_up_eta'])
-            for candidate in sorted_candidates:
-                if candidate['pick_up_eta'] > 600:
-                    break
+                # TODO: penalize cancellation rate
+                # Best incremental improvement (get the ride AND improve driver position)
+                value = reward + self.gamma * v1 - v0
+                if value > best_update:
+                    best_update, best_candidate = value, candidate
 
-                if candidate['driver_id'] not in assigned_drivers:
-                    assigned_drivers.add(candidate['driver_id'])
-                    dispatch_action.append(dict(order_id=order_id, driver_id=candidate['driver_id']))
-                    break
+            # Assign driver
+            if best_candidate:
+                assigned_driver_ids.add(best_candidate.driver_id)
+                dispatch[request.request_id] = best_candidate
+                driver = drivers[best_candidate.driver_id]
+                self.state_values[driver.location] += self.alpha * best_update
 
-        return dispatch_action
+        # Decay idle driver positions
+        for driver in drivers.values():
+            if driver.driver_id in assigned_driver_ids:
+                continue
+            v0 = self.state_values[driver.location]
+            # TODO: use idle transition probabilities
+            v1 = self.state_values[driver.location]  # Driver hasn't moved if idle
+            self.state_values[driver.location] += self.alpha + (self.idle_cost + self.gamma * v1 - v0)
 
-    def reposition(self, repo_observ):
+        self.last_dispatch = dispatch
+        return [dict(order_id=order_id, driver_id=d.driver_id) for order_id, d in dispatch.items()]
+
+    def reposition(self, reposition_input):
         """ Compute the reposition action for the given drivers
-    :param repo_observ: a dict, the key in the dict includes:
+    :param reposition_input: a dict, the key in the dict includes:
         timestamp: int
         driver_info: a list of dict, the key in the dict includes:
             driver_id: driver_id of the idle driver in the treatment group, int
@@ -78,7 +96,8 @@ class Agent(object):
         destination: id of the grid the driver is repositioned to, str
     """
         repo_action = []
-        for driver in repo_observ['driver_info']:
+        data = RepositionData(reposition_input)
+        for driver in reposition_input['driver_info']:
             # the default reposition is to let drivers stay where they are
             repo_action.append(dict(driver_id=driver['driver_id'], destination=driver['grid_id']))
         return repo_action
---
+++ mobility_on_demand/model/utils.py
@@ -0,0 +1,68 @@
+import collections
+from typing import Any, Dict, Set, Tuple
+
+
+LAT_RANGE = (30.652828, 30.727818)
+LNG_RANGE = (104.042102, 104.129591)
+
+class Driver:
+    def __init__(self, od, h3_resolution):
+        self.driver_id = od['driver_id']
+        self.location = loc_to_grid(od['driver_location'], h3_resolution)
+
+    def __repr__(self):
+        return f'Driver:{self.driver_id}@{self.location}'
+
+
+class Request:
+    def __init__(self, od, h3_resolution):
+        self.request_id = od['order_id']
+        self.start_loc = loc_to_grid(od['order_start_location'], h3_resolution)
+        self.end_loc = loc_to_grid(od['order_finish_location'], h3_resolution)
+        self.request_ts = od['timestamp']
+        self.finish_ts = od['order_finish_timestamp']
+        self.day_of_week = od['day_of_week']
+        self.reward = od['reward_units']
+
+    def __repr__(self):
+        return f'Request:{self.request_id},{self.start_loc}-{self.end_loc}:{self.reward}'
+
+
+class DispatchCandidate:
+    def __init__(self, od):
+        self.driver_id = od['driver_id']
+        self.request_id = od['order_id']
+        self.distance = od['order_driver_distance']
+        self.eta = od['pick_up_eta']
+
+    def __repr__(self):
+        return f'Candidate:{self.driver_id},{self.request_id}:{self.distance},{self.eta}'
+
+
+class RepositionData:
+    def __init__(self, r):
+        self.timestamp = r['timestamp']
+        self.driver_ids = []
+        for d in r['driver_info']:
+            # Didi GridID is also available as d['grid_id]
+            self.driver_ids.append(d['driver_id'])
+        self.day_of_week = r['day_of_week']
+
+def parse_dispatch(dispatch_input: Dict[str, Any], h3_resolution: int) -> (Dict[str, Driver], Dict[str, Request], Dict[str, Set[DispatchCandidate]]):
+    drivers = dict()
+    requests = dict()
+    candidates = collections.defaultdict(set)
+    for candidate in dispatch_input:
+        driver = Driver(candidate, h3_resolution)
+        drivers[driver.driver_id] = driver
+        request = Request(candidate, h3_resolution)
+        requests[request.request_id] = request
+        candidates[request.request_id].add(DispatchCandidate(candidate))
+    return drivers, requests, candidates
+
+
+def loc_to_grid(location: Tuple[float, float], h3_resolution: int) -> str:
+    # TODO: Convert to Didi grid
+    # Apparently we can't use libraries...
+    # h3.geo_to_h3(location[1], location[0], h3_resolution)
+    return f'{location[1]:0.2f},{location[0]:0.2f}'
