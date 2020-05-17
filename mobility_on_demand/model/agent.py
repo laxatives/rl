b35a3f48154cc8f40689f6eda58fc1577aa29419
Benjamin Han <laxatives@users.noreply.github.com>
2020-05-17 12:08:18 -0700

    Clean up

--- mobility_on_demand/model/agent.py
+++ mobility_on_demand/model/agent.py
@@ -16,15 +16,11 @@ class Agent:
     """ Agent for dispatching and repositioning drivers for the 2020 ACM SIGKDD Cup Competition """
     def __init__(self, alpha=2/(5*60), gamma=0.9, idle_reward=-2/(60*60)):
         self.dispatcher = Dql(alpha, gamma, idle_reward)
-        self.last_dispatch = {}
-        self.last_reposition = {}
 
     def dispatch(self, dispatch_input):
         """ Compute the assignment between drivers and passengers at each time step """
         drivers, requests, candidates = utils.parse_dispatch(dispatch_input)
         dispatch = self.dispatcher.dispatch(drivers, requests, candidates)
-
-        self.last_dispatch = dispatch
         return [dict(order_id=order_id, driver_id=d.driver_id) for order_id, d in dispatch.items()]
 
     def reposition(self, reposition_input):
@@ -35,5 +31,4 @@ class Agent:
             reposition.append(dict(driver_id=driver_id, destination=position_id))
 
         # TODO: Brute-force expected reward, discounted by ETA
-
         return reposition
