47b3fba7a38b8ed646bd3f8595a3322cd4e61268
Benjamin Han <laxatives@users.noreply.github.com>
2020-05-17 15:01:28 -0700

    Iterate candidates

--- mobility_on_demand/model/dispatch.py
+++ mobility_on_demand/model/dispatch.py
@@ -1,7 +1,7 @@
 import collections
 import random
 from abc import abstractmethod
-from typing import Dict, Set
+from typing import Dict, List, Set, Tuple
 
 from utils import DispatchCandidate, Driver, Request
 
@@ -21,6 +21,12 @@ class Dispatcher:
         ...
 
 
+class ScoredCandidate:
+    def __init__(self, candidate: DispatchCandidate, score: int):
+        self.candidate = candidate
+        self.score = score
+
+
 class Sarsa(Dispatcher):
     def __init__(self, alpha, gamma, idle_reward):
         super().__init__(alpha, gamma, idle_reward)
@@ -28,37 +34,33 @@ class Sarsa(Dispatcher):
 
     def dispatch(self, drivers: Dict[str, Driver], requests: Dict[str, Request],
               candidates: Dict[str, Set[DispatchCandidate]]) -> Dict[str, DispatchCandidate]:
+        ranking = []  # type: List[ScoredCandidate]
+        for candidate in set(c for cs in candidates.values() for c in cs):  # type: DispatchCandidate
+            request = requests[candidate.request_id]
+            driver = drivers[candidate.driver_id]
+
+            v0 = self.state_values[driver.location]  # Value of the driver current position
+            v1 = self.state_values[request.end_loc]  # Value of the proposed new position
+            reward = request.reward
+
+            # TODO: penalize cancellation rate
+            # Best incremental improvement (get the ride AND improve driver position)
+            update = reward + self.gamma * v1 - v0
+            ranking.append(ScoredCandidate(candidate, update))
+
+        # Assign drivers
         assigned_driver_ids = set()  # type: Set[str]
         dispatch = dict()  # type: Dict[str, DispatchCandidate]
+        for scored in sorted(ranking, key=lambda x: x.score, reverse=True):  # type: ScoredCandidate
+            candidate = scored.candidate
+            if candidate.request_id in dispatch or candidate.driver_id in assigned_driver_ids:
+                continue
+            assigned_driver_ids.add(candidate.driver_id)
 
-        # TODO: Iterate over candidates instead of requests
-        # Greedily match highest reward requests first
-        for request in sorted(requests.values(), key=lambda x: x.reward, reverse=True):  # type: Request
-            best_candidate: DispatchCandidate = None
-            best_update: int = 0  # Wait if value is negative
-
-            for candidate in sorted(candidates[request.request_id], key=lambda x: x.eta,
-                                    reverse=True):  # type: DispatchCandidate
-                if candidate.driver_id in assigned_driver_ids:
-                    continue
-
-                driver = drivers[candidate.driver_id]
-                v0 = self.state_values[driver.location]  # Value of the driver current position
-                v1 = self.state_values[request.end_loc]  # Value of the proposed new position
-                reward = request.reward
-
-                # TODO: penalize cancellation rate
-                # Best incremental improvement (get the ride AND improve driver position)
-                update = reward + self.gamma * v1 - v0
-                if update > best_update:
-                    best_update, best_candidate = update, candidate
-
-            # Assign driver
-            if best_candidate:
-                assigned_driver_ids.add(best_candidate.driver_id)
-                dispatch[request.request_id] = best_candidate
-                driver = drivers[best_candidate.driver_id]
-                self.state_values[driver.location] += self.alpha * best_update
+            request = requests[candidate.request_id]
+            dispatch[request.request_id] = candidate
+            driver = drivers[candidate.driver_id]
+            self.state_values[driver.location] += self.alpha * scored.score
 
         # Reward (negative) for idle driver positions
         for driver in drivers.values():
@@ -81,71 +83,58 @@ class Dql(Dispatcher):
         self.values_left = collections.defaultdict(int)
         self.values_right = collections.defaultdict(int)
 
-
-    def _joint_policy(self, request, candidates, drivers, assigned_driver_ids):
-        best_candidate: DispatchCandidate = None
-        best_update: int = 0  # Wait if value is negative
-
-        for candidate in sorted(candidates[request.request_id], key=lambda x: x.eta,
-                                reverse=True):  # type: DispatchCandidate
-            if candidate.driver_id in assigned_driver_ids:
-                continue
-
-            driver = drivers[candidate.driver_id]
-            v0 = self.state_value(driver.location)
-            v1 = self.state_value(request.end_loc)
-            reward = request.reward
-
-            # TODO: penalize cancellation rate
-            # Best incremental improvement (get the ride AND improve driver position)
-            update = reward + self.gamma * v1 - v0
-            if update > best_update:
-                best_update, best_candidate = update, candidate
-        return best_candidate
-
-
     def dispatch(self, drivers: Dict[str, Driver], requests: Dict[str, Request],
                  candidates: Dict[str, Set[DispatchCandidate]]) -> Dict[str, DispatchCandidate]:
+        #  Flip a coin
+        if random.random() < 0.5:
+            student, teacher = self.values_left, self.values_right
+        else:
+            student, teacher = self.values_right, self.values_left
+
+        updates = dict()  # type: Dict[Tuple(str, str), ScoredCandidate]
+        ranking = []  # type: List[ScoredCandidate]
+        for candidate in set(c for cs in candidates.values() for c in cs):  # type: DispatchCandidate
+            # Teacher provides the value estimate
+            request = requests[candidate.request_id]
+            q1 = teacher[request.end_loc]
+
+            # Compute student update for every candidate
+            driver = drivers[candidate.driver_id]
+            q0 = student[driver.location]
+            update = request.reward + self.gamma * q1 - q0
+            updates[(candidate.request_id, candidate.driver_id)] = ScoredCandidate(candidate, update)
+
+            # Joint Ranking
+            q0 = self.state_value(driver.location)
+            q1 = self.state_value(request.end_loc)
+            joint_update = request.reward + self.gamma * q1 - q0
+            ranking.append(ScoredCandidate(candidate, joint_update))
+
+        # Assign drivers
         assigned_driver_ids = set()  # type: Set[str]
         dispatch = dict()  # type: Dict[str, DispatchCandidate]
+        for scored in sorted(ranking, key=lambda x: x.score, reverse=True):  # type: ScoredCandidate
+            candidate = scored.candidate
+            if candidate.request_id in dispatch or candidate.driver_id in assigned_driver_ids:
+                continue
+            assigned_driver_ids.add(candidate.driver_id)
 
-        # TODO: Iterate over candidates instead of requests
-        # Greedily match highest reward requests first
-        for request in sorted(requests.values(), key=lambda x: x.reward, reverse=True):  # type: Request
-            for candidate in sorted(candidates[request.request_id], key=lambda x: x.eta,
-                                    reverse=True):  # type: DispatchCandidate
-                if candidate.driver_id in assigned_driver_ids:
-                    continue
+            request = requests[candidate.request_id]
+            dispatch[request.request_id] = candidate
+            driver = drivers[candidate.driver_id]
 
-                # Flip a coin
-                if random.random() < 0.5:
-                    student, teacher = self.values_left, self.values_right
-                else:
-                    student, teacher = self.values_right, self.values_left
-
-                # Teacher provides the value estimate
-                q1 = teacher[request.end_loc]
-
-                # Update the student
-                driver = drivers[candidate.driver_id]
-                q0 = student[driver.location]
-                student[driver.location] += self.alpha * (request.reward + self.gamma * q1 - q0)
-
-            # Assign driver
-            selected = self._joint_policy(request, candidates, drivers, assigned_driver_ids)
-            if selected:
-                assigned_driver_ids.add(selected.driver_id)
-                dispatch[request.request_id] = selected
+            # Update student for selected candidate
+            update = updates[(candidate.request_id, candidate.driver_id)].score
+            student[driver.location] += self.alpha * update
 
         # Reward (negative) for idle driver positions
         for driver in drivers.values():
             if driver.driver_id in assigned_driver_ids:
                 continue
-            values = random.choice((self.values_left, self.values_right))
-            v0 = values[driver.location]
+            v0 = student[driver.location]
             # TODO: use idle transition probabilities
-            v1 = values[driver.location]  # Assume driver hasn't moved if idle
-            values[driver.location] += self.alpha * (self.idle_reward + self.gamma * v1 - v0)
+            v1 = teacher[driver.location]  # Assume driver hasn't moved if idle
+            student[driver.location] += self.alpha * (self.idle_reward + self.gamma * v1 - v0)
         return dispatch
 
     def state_value(self, grid_id: str) -> int:
