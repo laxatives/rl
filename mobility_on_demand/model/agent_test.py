4cea82169a1af5f075b5f389394fc971bfed7278
Benjamin Han <laxatives@users.noreply.github.com>
2020-05-21 21:40:45 -0700

    Imports

--- mobility_on_demand/model/agent_test.py
+++ mobility_on_demand/model/agent_test.py
@@ -9,16 +9,18 @@ SAMPLE_DIR = os.path.abspath('../samples')
 
 
 class AgentTest(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         with open(os.path.join(SAMPLE_DIR, 'dispatch_observ'), 'r') as f:
-            self.dispatch_observ = json.load(f)
+            cls.dispatch_observ = json.load(f)
         with open(os.path.join(SAMPLE_DIR, 'repo_observ'), 'r') as f:
-            self.repo_observ = json.load(f)
-
+            cls.repo_observ = json.load(f)
 
     def test_agent(self):
+        agent = Agent()
+
         for _ in range(3):
-            d = Agent().dispatch(self.dispatch_observ)
+            d = agent.dispatch(self.dispatch_observ)
             assert d
-            r = Agent().reposition(self.repo_observ)
+            r = agent.reposition(self.repo_observ)
             assert r
