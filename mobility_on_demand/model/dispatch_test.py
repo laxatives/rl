63a01e83e2c899ff6ae25debb6f46762fa0e3915
Benjamin Han <laxatives@users.noreply.github.com>
2020-05-21 22:24:03 -0700

    Cleanup

--- mobility_on_demand/model/dispatch_test.py
+++ mobility_on_demand/model/dispatch_test.py
@@ -6,6 +6,9 @@ import dispatch
 import parse
 
 
+MEAN_CANCEL_RATES = [0.03493870431607338, 0.03866776293519174, 0.041760728528424544, 0.05007157148698522,
+                     0.059208628863229744, 0.07455933064560377, 0.08571890195014424, 0.09848048263719175,
+                     0.11230701971967454, 0.12717324794320947]
 SAMPLE_DIR = os.path.abspath('../samples')
 
 
@@ -40,7 +43,7 @@ class DispatchTest(unittest.TestCase):
         for i in range(10):
             distance = 200 + i * 200
             rate = 1 - dispatch.completion_rate(distance)
-            assert abs(rate - dispatch.MEAN_CANCEL_RATES[i]) < 0.01, rate
+            assert abs(rate - MEAN_CANCEL_RATES[i]) < 0.01, rate
 
         rate = dispatch.completion_rate(5000)
         assert 0 <= rate < 1e-6, rate
