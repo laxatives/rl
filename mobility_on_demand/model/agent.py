c7e466c7b835a45595d2ec3dac57f150491f0ce7
laxatives <laxatives@users.noreply.github.com>
2020-06-18 14:44:17 -0700

    Revert "stRevert "Ride cancel update""
    This reverts commit e25f32f40bddc97a8e898a2accdcf7e5bfb6a96b.

--- mobility_on_demand/model/agent.py
+++ mobility_on_demand/model/agent.py
@@ -11,7 +11,7 @@ import reposition as repositioner
 
 class Agent:
     """ Agent for dispatching and repositioning drivers for the 2020 ACM SIGKDD Cup Competition """
-    def __init__(self, alpha=20/(5*60), dispatch_gamma=0.9, reposition_gamma=0.99):
+    def __init__(self, alpha=20/(5*60), dispatch_gamma=0.9, reposition_gamma=0.99, **kwargs):
         self.dispatcher = dispatcher.Sarsa(alpha, dispatch_gamma)
         self.repositioner = repositioner.StateValueGreedy(self.dispatcher, reposition_gamma)
 
